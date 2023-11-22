2fbbf5294a85682b5579ddbf1b34d035ffae71ab
pn-1705 <99930206+pn-1705@users.noreply.github.com>
2023-11-22 21:57:03 +0700

    update 22-11

--- resources/views/user/elements/header.blade.php
+++ resources/views/user/elements/header.blade.php
@@ -28,14 +28,10 @@
                             <ul>
                                 <li><a href="#">Deep Drop Down 1</a></li>
                                 <li><a href="#">Deep Drop Down 2</a></li>
-                                <li><a href="#">Deep Drop Down 3</a></li>
-                                <li><a href="#">Deep Drop Down 4</a></li>
-                                <li><a href="#">Deep Drop Down 5</a></li>
+
                             </ul>
                         </li>
                         <li><a href="#">Drop Down 2</a></li>
-                        <li><a href="#">Drop Down 3</a></li>
-                        <li><a href="#">Drop Down 4</a></li>
                     </ul>
                 </li>
                 <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
@@ -44,42 +40,54 @@
                     if (Auth::check() == false) {
                         echo 'hidden';
                     }
-                    ?> style="white-space: nowrap;padding: 10px;"><a class="" href=""><i class="bi bi-bell"></i></a>
+                    ?>><a href="#" title="Thông Báo Việc Làm"><span class="mdi mdi-bell">&nbsp;Thông báo</span></a>
                 </li>
-                <li style="white-space: nowrap;padding: 10px 0 10px 10px;" class="dropdown">
-                    <a class="{{ Route::is('information') ? 'active' : '' }}"
-                       href="{{ route('user.pages.login_page') }}">
-                        <span class="bi bi-person"></span> &nbsp;
+                <li
+                    <?php
+                    if (Auth::check() == true) {
+                        echo 'hidden';
+                    }
+                    ?>><a class="nav-link scrollto" href="{{ route('user.pages.login_page') }}"><span class="mdi mdi-account-circle">&nbsp;Đăng nhập</span></a>
+                </li>
+                <li class="dropdown" <?php
+                    if (Auth::check() == false) {
+                        echo 'hidden';
+                    }
+                    ?>>
+                    <a href="/" class="{{ Route::is('information') ? 'active' : '' }} nav-link scrollto">
                         <?php
-                        //                        $info = Auth::user()->ten;
                         if (isset(Auth::user()->id)) {
-                            echo Auth::user()->ten;
-                            echo '<i class="bi bi-chevron-down"></i></a>';
-                        } else
-                            echo 'Đăng nhập';
+                            echo '<span class="mdi mdi-account-circle">&nbsp;' . Auth::user()->ten . '</span>';
+//                            echo Auth::user()->ten;
+                            echo '<i class="bi bi-chevron-down"></i>';
+                        }
                         ?>
-                        <ul
-                        <?php
-                            if (empty(Auth::user()->id)) {
-                                echo 'hidden';
-                            }
-                            ?>
-                        >
-
+                    </a>
+                        <ul>
                             @if(isset(Auth::user()->id_nhomquyen))
                                 @if(Auth::user()->id_nhomquyen == 0)
-                                    <li><a style="font-weight: 700;" href="{{ route('profile') }}"> &nbsp; Quản Lí Hồ Sơ</a></li>
-                                    <li><a style="font-weight: 700;" href="{{ route('CV') }}"> &nbsp; Hồ sơ xin việc</a></li>
-                                    <li><a style="font-weight: 700;" href="{{ route('viec-lam-da-luu') }}"> &nbsp; Việc làm đã lưu</a></li>
-                                    <li><a style="font-weight: 700;" href="{{ route('viec-lam-da-nop') }}"> &nbsp; Việc làm đã ứng tuyển</a></li>
-                                    <li><a style="font-weight: 700;" href="{{ route('information') }}"> &nbsp; Tài khoản</a></li>
+                                    <li><a style="font-weight: 700;" href="{{ route('profile') }}"> &nbsp; Quản Lí Hồ
+                                            Sơ</a></li>
+                                    <li><a style="font-weight: 700;" href="{{ route('CV') }}"> &nbsp; Hồ sơ xin việc</a>
+                                    </li>
+                                    <li><a style="font-weight: 700;" href="{{ route('viec-lam-da-luu') }}"> &nbsp; Việc
+                                            làm đã lưu</a></li>
+                                    <li><a style="font-weight: 700;" href="{{ route('viec-lam-da-nop') }}"> &nbsp; Việc
+                                            làm đã ứng tuyển</a></li>
+                                    <li><a style="font-weight: 700;" href="{{ route('information') }}"> &nbsp; Tài
+                                            khoản</a></li>
                                 @endif
                                 @if(Auth::user()->id_nhomquyen == 1)
-                                    <li><a style="font-weight: 700;" href="{{ route('employer.view_hrcentral') }}"> &nbsp; Dashboard</a></li>
-                                    <li><a style="font-weight: 700;" href="{{ route('employer.view_hrcentral') }}"> &nbsp; Quản lí đăng tuyển</a></li>
-                                    <li><a style="font-weight: 700;" href="{{ route('employer.view_hrcentral') }}"> &nbsp; Quản lí ứng viên</a></li>
-                                        <li><a style="font-weight: 700;" href="{{ route('employer.view_company_info') }}"> &nbsp; Thông tin công ty</a></li>
-                                        <li><a style="font-weight: 700;" href="{{ route('employer.view_account') }}"> &nbsp; Tài khoản</a></li>
+                                    <li><a style="font-weight: 700;" href="{{ route('employer.view_hrcentral') }}">
+                                            &nbsp; Dashboard</a></li>
+                                    <li><a style="font-weight: 700;" href="{{ route('employer.view_hrcentral') }}">
+                                            &nbsp; Quản lí đăng tuyển</a></li>
+                                    <li><a style="font-weight: 700;" href="{{ route('employer.view_hrcentral') }}">
+                                            &nbsp; Quản lí ứng viên</a></li>
+                                    <li><a style="font-weight: 700;" href="{{ route('employer.view_company_info') }}">
+                                            &nbsp; Thông tin công ty</a></li>
+                                    <li><a style="font-weight: 700;" href="{{ route('employer.view_account') }}"> &nbsp;
+                                            Tài khoản</a></li>
                                 @endif
                             @endif
 
@@ -91,10 +99,7 @@
                     if (isset(Auth::user()->id)) {
                         echo 'hidden';
                     }
-                    ?> style="white-space: nowrap;padding: 10px;"><a class="nav-link scrollto"
-                                                                     href="{{route('user.pages.register_page')}}"><span
-                            class="bi bi-person-add"></span>&nbsp; Đăng kí</a>
-                </li>
+                    ?> ><a class="nav-link scrollto" href="{{route('user.pages.register_page')}}"><span class="">Đăng ký</span></a></li>
             </ul>
             <div class="button-hambuger">
                 <i class="bi bi-list mobile-nav-toggle"></i>
@@ -103,7 +108,7 @@
     </div>
 </header><!-- End Header -->
 <style>
-    @media screen and (max-width: 991px) {
+    @media screen and (max-width: 1200px) {
         #logo {
             -webkit-transform: translate(-50%, -50%);
             -ms-transform: translate(-50%, -50%);
@@ -112,6 +117,16 @@
             left: 50%;
             padding-left: 0;
             transform: translate(-50%, -50%);
+        }
+    }
+    @media (max-width: 1200px) {
+        .mobile-nav-toggle {
+            display: block;
+        }
+        .navbar ul {
+            display: none;
+        }.navbar-mobile ul {
+            display: block;
         }
     }
 </style>
